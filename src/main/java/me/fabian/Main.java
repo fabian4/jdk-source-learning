cce0aad16872f1498e7042e5b56a191c6d835c69
丁生 <fabian4@163.com>
2021-09-19 23:14:02 +0800

    adjust directory structure

--- src/main/java/me/fabian/Main.java
+++ src/main/java/me/fabian/Main.java
@@ -1,11 +1,19 @@
 package me.fabian;
 
+import java.util.HashMap;
+
 /**
  * @author fabian
  * @date 2021-09-19 21:24
  */
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>(8);
 
+        map.put("1", "d");
+        map.put("5", "d");
+        map.put("4", "d");
+        map.put("3", "d");
+        map.put("2", "d");
     }
 }
