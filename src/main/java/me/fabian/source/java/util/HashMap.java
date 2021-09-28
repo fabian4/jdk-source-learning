d9674c4b8cf316b7f6482b8b643e69d6a5690733
丁生 <fabian4@163.com>
2021-09-28 23:17:48 +0800

    update

--- src/main/java/me/fabian/source/java/util/HashMap.java
+++ src/main/java/me/fabian/source/java/util/HashMap.java
@@ -571,15 +571,26 @@ public class HashMap<K,V> extends AbstractMap<K,V>
      * @return the node, or null if none
      */
     final Node<K,V> getNode(int hash, Object key) {
-        Node<K,V>[] tab; Node<K,V> first, e; int n; K k;
+
+        Node<K,V>[] tab;
+        Node<K,V> first, e;
+        int n;
+        K k;
+
         if ((tab = table) != null && (n = tab.length) > 0 &&
             (first = tab[(n - 1) & hash]) != null) {
+
+            // 先对头节点做判断
             if (first.hash == hash && // always check first node
                 ((k = first.key) == key || (key != null && key.equals(k))))
                 return first;
             if ((e = first.next) != null) {
+
+                // 如果是红黑树结点
                 if (first instanceof TreeNode)
                     return ((TreeNode<K,V>)first).getTreeNode(hash, key);
+
+                // 最后遍历链表
                 do {
                     if (e.hash == hash &&
                         ((k = e.key) == key || (key != null && key.equals(k))))
